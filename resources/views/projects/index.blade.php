e4889d2355e2000b584613120bccddbea78e9747
Haaragard <diogololz159@gmail.com>
2022-03-29 23:00:24 -0300

    Feature - Styled projects index

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,23 +2,25 @@
     <x-slot name="header"></x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 w-full">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="flex items-center justify-between">
-                        <h1 class="bold text-xl">Birdboard</h1>
-                        <a href="/projects/create" class="underline">New project</a>
+                <div class="p-6 bg-white border-b border-gray-200 flex flex-col">
+
+                    <div class="flex items-center justify-between mb-3">
+                        <h2 class="text-sm text-gray-500">My Projects</h2>
+                        <a href="/projects/create" class="text-gray-500">New project</a>
                     </div>
 
-                    <ul class="mt-2">
+                    <div class="grid grid-cols-3 gap-4">
                         @forelse ($projects as $project)
-                            <li>
-                                <a href="{{ $project->path() }}" class="underline">{{ $project->title }}</a>
-                            </li>
+                            <div class="w-full rounded border shadow p-5">
+                                <h3 class="text-xl font-semibold py-4">{{ $project->title }}</h3>
+                                <div class="text-gray-400 font-light">{{ Str::limit($project->description, 300) }}</div>
+                            </div>
                         @empty
-                            <li>No projects yet.</li>
+                            <p class="bold text-xl">No projects yet.</p>
                         @endforelse
-                    </ul>
+                    </div>
                 </div>
             </div>
         </div>
